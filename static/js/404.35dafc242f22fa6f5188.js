2a9fbc52a49948ab7f28fe7a5c4751309bbfd58d
Tidyzq <tidyzq@tidyzq.com>
2017-05-23 14:30:30 +0800

    Updates

--- static/js/404.35dafc242f22fa6f5188.js
+++ static/js/404.35dafc242f22fa6f5188.js
@@ -1,24 +1,2 @@
-webpackJsonp([3],{
-
-/***/ 43:
-/***/ (function(module, exports) {
-
-function redirect(l) {
-  var pathnames = l.pathname.split('/');
-  var repo = pathnames[1];
-  var pathname = pathnames.slice(2).join('/');
-  var host = l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + '/' + repo + '/';
-  var path = pathname.replace(/&/g, '~and~');
-  var search = l.search.slice(1).replace(/&/g, '~and~');
-  var hash = l.hash;
-
-  var url = host + '?p=' + path + '&q=' + search + (hash ? '#' + hash : '');
-  l.replace(url);
-}
-
-redirect(window.location);
-
-/***/ })
-
-},[43]);
+webpackJsonp([3],{43:function(a,o){!function(a){var o=a.pathname.split("/"),e=o[1],n=o.slice(2).join("/"),c=a.protocol+"//"+a.hostname+(a.port?":"+a.port:"")+"/"+e+"/",p=n.replace(/&/g,"~and~"),t=a.search.slice(1).replace(/&/g,"~and~"),i=a.hash,l=c+"?p="+p+"&q="+t+(i?"#"+i:"");a.replace(l)}(window.location)}},[43]);
 //# sourceMappingURL=404.35dafc242f22fa6f5188.js.map
